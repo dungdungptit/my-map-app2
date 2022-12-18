c065526cf80708e40dad2d10b4632f8ced4d63e5
Đặng Quang Dũng <92563380+dungdungptit@users.noreply.github.com>
2022-12-19 02:48:17 +0700

    ws

--- src/feature/Map/WebSocket.js
+++ src/feature/Map/WebSocket.js
@@ -13,7 +13,7 @@ const isDriver = () => {
 const auth = JSON.parse(localStorage.getItem('user'));
 
 if(isDriver()) {
-  websocket = new WebSocket(`ws://26.188.5.187:5000?id=vehicle_${auth?.vehicle?.id}`)
+  websocket = new WebSocket(`ws://map-ws-exp.cleverapps.io?id=vehicle_${auth?.vehicle?.id}`)
 }
 
 let connection_resolvers = [];
--- src/ultils/socketApi.js
+++ src/ultils/socketApi.js
@@ -1,2 +1,2 @@
 // export const BASE_URL_SOCKET = 'ws://26.188.5.187:5000?id=admin_0'
-export const BASE_URL_SOCKET = 'https://map-ws-exp.cleverapps.io'
+export const BASE_URL_SOCKET = 'ws://map-ws-exp.cleverapps.io?id=admin_0'
