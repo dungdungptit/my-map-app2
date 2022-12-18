c7544c2df86d687d5a1aa87cdce15a8426bd7716
Đặng Quang Dũng <92563380+dungdungptit@users.noreply.github.com>
2022-12-19 03:30:45 +0700

    css fix

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -172,19 +172,16 @@ const DataTable = ({ rows, columns }) => {
                 "& .MuiDataGrid-cell:focus-within, & .MuiDataGrid-cell:focus": {
                     outline: "none"
                 },
-                '& .css-1j9kmqg-MuiDataGrid-toolbarContainer': {
+                '& .MuiDataGrid-toolbarContainer': {
                     padding: '12px 16px 8px',
                     borderBottom: '1px solid #e0e0e0',
                 },
-                '& .css-c63i49-MuiInputBase-input-MuiInput-input': {
+                '& .MuiInputBase-input': {
                     padding: '8px 0px',
                     fontSize: '0.875rem',
                     lineHeight: '1.25rem',
                     fontWeight: '400',
                     letterSpacing: '0.01071em',
-                },
-                '& .css-levciy-MuiTablePagination-displayedRows': {
-                    mb: 0
                 },
             }}
             rowCount={rows.length}
