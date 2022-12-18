2e94406446cac9998bd89b3be90b54ba06b45ef5
Đặng Quang Dũng <92563380+dungdungptit@users.noreply.github.com>
2022-12-19 02:33:01 +0700

    config

--- src/ultils/socketApi.js
+++ src/ultils/socketApi.js
@@ -1,1 +1,2 @@
-export const BASE_URL_SOCKET = 'ws://26.188.5.187:5000?id=admin_0'
+// export const BASE_URL_SOCKET = 'ws://26.188.5.187:5000?id=admin_0'
+export const BASE_URL_SOCKET = 'https://map-ws-exp.cleverapps.io'
